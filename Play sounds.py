a58dd6078a192c12612eb3451ebeebcf428e14c3
AhmedOssama222 <ahmedossama22@outlook.com>
2017-03-19 16:52:57 -0700

    Update Play sounds.py

--- Play sounds.py
+++ Play sounds.py
@@ -1,4 +1,7 @@
 import winsound
+import time
+
 while True:
-    winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
+    winsound.PlaySound("132106__sironboy__woman-scream", winsound.SND_ALIAS)
     winsound.PlaySound("*", winsound.SND_ALIAS)
+    time.sleep(15)
